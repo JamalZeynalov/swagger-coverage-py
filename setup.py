d76a4fe42e4cbdabd6a75ebb36be5d170896c741
Jamal <jamal.zeynalov@gmail.com>
2021-02-02 13:39:51 +0200

    release 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="swagger-coverage",
-    version="0.0.5",
+    version="1.0.0",
     author="Jamal Zeinalov",
     author_email="jamal.zeynalov@gmail.com",
     description='Python adapter for "swagger-coverage" tool',
