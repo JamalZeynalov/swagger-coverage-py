7907fa2e1c32e50aa1493eee03f17eb875b1a7d4
Jamal <jamal.zeynalov@gmail.com>
2021-10-22 15:23:23 +0300

    Support OpenAPI 3.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="swagger-coverage",
-    version="2.0.8",
+    version="2.0.9",
     author="Jamal Zeinalov",
     author_email="jamal.zeynalov@gmail.com",
     description='Python adapter for "swagger-coverage" tool',
