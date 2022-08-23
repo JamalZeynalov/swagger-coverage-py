f84e87de81ac8f692a45a301da1877308c010194
Jamal <jamal.zeynalov@gmail.com>
2022-08-23 12:50:22 +0300

    Fix configs

--- swagger_coverage_py/configs.py
+++ swagger_coverage_py/configs.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(f"{rootpath.detect()}/.env")
 env = Env()
 
-API_DOCS_TYPE = env.st("API_DOCS_TYPE", default="openapi")  # "swagger"
-API_DOCS_VERSION = env.st("API_DOCS_VERSION", default="3.0.0")  # "2.0"
+API_DOCS_TYPE = env.str("API_DOCS_TYPE", default="openapi")  # "swagger"
+API_DOCS_VERSION = env.str("API_DOCS_VERSION", default="3.0.0")  # "2.0"
 API_DOCS_FORMAT = env.str("API_DOCS_FORMAT", default="json")  # "yaml"
 IS_DISABLED = env.bool("API_COVERAGE_REPORTS_DISABLED", default=False)  # If True then requests won't be recorded
