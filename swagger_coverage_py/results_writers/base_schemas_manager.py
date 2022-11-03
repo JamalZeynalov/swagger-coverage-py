c818a7550d01fd0973f7ec88f6023f4b21a64d57
kipshenn <kipshenn@ya.ru>
2022-11-03 12:35:30 +0300

    Add headers support

--- swagger_coverage_py/results_writers/base_schemas_manager.py
+++ swagger_coverage_py/results_writers/base_schemas_manager.py
@@ -41,20 +41,31 @@ class ApiDocsManagerBase:
 
         return request_body
 
-    def _get_query_params(self) -> list:
-        q_params = list(self.__other_request_params.get("params", {}).items())
+    def _get_other_request_params(self, params_key: str, params_in: str) -> list:
+        params = list(self.__other_request_params.get(params_key, {}).items())
         raw = self._uri.raw.split("?")
         if len(raw) > 1:
-            q_params += [tuple(x.split("=")) for x in str(raw[1]).split("&")]
-        if not q_params:
+            params += [tuple(x.split("=")) for x in str(raw[1]).split("&")]
+        if not params:
             return []
 
         params_ = []
-        for key, value in q_params:
+        for key, value in params:
             params_.append(
-                {"name": key, "in": "query", "required": False, "x-example": urllib.parse.unquote(str(value))}
+                {
+                    "name": key,
+                    "in": params_in,
+                    "required": False,
+                    "x-example": urllib.parse.unquote(str(value))
+                }
             )
         return params_
+
+    def _get_query_params(self) -> list:
+        return self._get_other_request_params(params_key='params', params_in='query')
+
+    def _get_header_params(self) -> list:
+        return self._get_other_request_params(params_key='headers', params_in='header')
 
     def __get_output_subdir(self):
         return re.match(r"(^\w*)://(.*)", self._uri.host).group(2)
