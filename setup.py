bc27268f04e03142a13c8d50a1b9cd655e59acee
Jamal <jamal.zeynalov@gmail.com>
2022-12-04 11:14:39 +0200

    Version increment (2.2.11)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="swagger-coverage",
-    version="2.2.10",
+    version="2.2.11",
     author="Jamal Zeinalov",
     author_email="jamal.zeynalov@gmail.com",
     description='Python adapter for "swagger-coverage" tool',
