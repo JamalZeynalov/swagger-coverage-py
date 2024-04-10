361bf9312713d542c62a361e6a2522393957a333
NaveenChandar <12165092+navchandar@users.noreply.github.com>
2024-04-10 20:35:15 +0530

    Update reporter.py
    Adding better command handling for subprocess.run
    This function runs the command directly (without going through a shell).
    This fixes issues when folder path containing spaces and avoids shell injection vulnerabilities.

--- swagger_coverage_py/reporter.py
+++ swagger_coverage_py/reporter.py
@@ -73,27 +73,28 @@ class CoverageReporter:
 
     def generate_report(self):
         inner_location = "swagger-coverage-commandline/bin/swagger-coverage-commandline"
-
+        
         cmd_path = os.path.join(os.path.dirname(__file__), inner_location)
         assert Path(
             cmd_path
         ).exists(), (
             f"No commandline tools is found in following locations:\n{cmd_path}\n"
         )
-
+        command = [cmd_path, "-s", self.swagger_doc_file, "-i", self.output_dir]
         if self.swagger_coverage_config:
-            command = f"{cmd_path} -s {self.swagger_doc_file} -i {self.output_dir} -c {self.swagger_coverage_config}"
-        else:
-            command = f"{cmd_path} -s {self.swagger_doc_file} -i {self.output_dir}"
+            command.extend(["-c", self.swagger_coverage_config])
 
-        command = (
-            command if platform.system() != "Windows" else command.replace("/", "\\")
-        )
-
+        # Adjust the file paths for Windows
+        if platform.system() == "Windows":
+            command = [arg.replace("/", "\\") for arg in command]
+        
         # Suppress all output if not in debug mode
-        command = command + " > /dev/null 2>&1" if not DEBUG_MODE else command
+        if not DEBUG_MODE:
+            with open(os.devnull, 'w') as devnull:
+                subprocess.run(command, stdout=devnull, stderr=devnull)
+        else:
+            subprocess.run(command)
 
-        subprocess.run(command, shell=True)
 
     def cleanup_input_files(self):
         shutil.rmtree(self.output_dir, ignore_errors=True)
