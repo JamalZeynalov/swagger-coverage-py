595edd2e03986d95d42db3621e5e7dae4812aed7
Jamal <jamal.zeynalov@gmail.com>
2024-04-25 17:39:29 +0300

    fix

--- swagger_coverage_py/results_writers/openapi_schemas_manager.py
+++ swagger_coverage_py/results_writers/openapi_schemas_manager.py
@@ -14,7 +14,6 @@ class OpenApiSchemasManager(ApiDocsManagerBase):
         params = (
             self._get_path_params()
             + self._get_query_params()
-            + self._get_body_params()
             + self._get_header_params()
         )
         dict_ = {
